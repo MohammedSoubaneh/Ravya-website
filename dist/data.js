1ecf0535dc460d1044e1c3c6a0e4e79a95c0ed59
akmalelahi <akmalelahishaikh@gmail.com>
2020-10-16 08:13:53 +0500

    fix:babel transform-runtime error

---
+++ dist/data.js
@@ -0,0 +1,48 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  products: [{
+    _id: 'Ravya-25pc',
+    title: 'For Your Health',
+    name: 'Ravya Turmeric Infusion 25pc',
+    image: 'https://i.ibb.co/ZcrSD94/New25pc.jpg',
+    price: 20.00,
+    brand: 'Ravya',
+    countInStock: 500,
+    size: "500px",
+    headerTop: "30px",
+    spaceApart: "60px",
+    heroImage: '/images/productImage.jpg',
+    productWidth: '700px'
+  }, {
+    _id: 'Ravya-15pc',
+    title: 'For Your Creativity',
+    name: 'Ravya Turmeric Infusion 15pc',
+    image: 'https://i.ibb.co/cvLBdMc/New15pc.jpg',
+    price: 15.00,
+    brand: 'Ravya',
+    countInStock: 500,
+    size: "500px",
+    headerTop: "35px",
+    heroImage: '/images/productImage2.jpg',
+    productWidth: '700px'
+  }, {
+    _id: 'Ravya-2pc',
+    title: 'For Someone Special',
+    name: 'Ravya Turmeric Infusion 2pc',
+    image: 'https://i.ibb.co/bNGmSqb/New2pc.jpg',
+    price: 4.00,
+    brand: 'Ravya',
+    countInStock: 500,
+    size: "500px",
+    headerTop: "35px",
+    rightSpace: "60px",
+    heroImage: '/images/productImage3.jpg',
+    productWidth: '700px'
+  }]
+};
+exports["default"] = _default;
