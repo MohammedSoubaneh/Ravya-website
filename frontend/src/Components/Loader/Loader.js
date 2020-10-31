c1c59ef11316c6210985e1dc7cf144d5413189aa
akmalelahi <akmalelahishaikh@gmail.com>
2020-10-31 16:00:33 +0500

    add loader on service request

---
+++ frontend/src/Components/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './loader.css'
+const Loader = () => (
+  <div className='LoaderContainer'>
+    <div className='loader'>
+      <div className='lds-facebook'><div /><div /><div /></div>
+    </div>
+  </div>
+)
+
+export default Loader
