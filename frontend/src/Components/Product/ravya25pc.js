056dabdd3d2516392492b2d67c49311673569eba
akmalelahi <akmalelahishaikh@gmail.com>
2020-11-07 00:33:00 +0500

    comment react player

--- frontend/src/Components/Product/ravya25pc.js
+++ frontend/src/Components/Product/ravya25pc.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { detailsProduct } from '../../actions/productAction';
 import {motion} from 'framer-motion';
-import ReactPlayer from 'react-player';
+// import ReactPlayer from 'react-player';
 import VideoPlayer from 'react-video-js-player';
 
 
